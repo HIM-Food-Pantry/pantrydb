d1f9bbd1bf14084f5071d50507c5c59b36e86817
Nicholas Romero <ncrmro@gmail.com>
2016-10-02 11:36:57 -0500

    pantrydb-6 Removes logging as we will let heroku handle that.

--- src/him_database/settings/development.py
+++ src/him_database/settings/development.py
@@ -1,6 +1,3 @@
-import logging.config
-import sys
-
 import dj_database_url
 
 from .base import *  # NOQA
@@ -24,50 +21,3 @@ DATABASES = {
         'NAME': 'db.sqlite3',
     }
 }
-
-# Log everything to the logs directory at the top
-LOGFILE_ROOT = join(dirname(BASE_DIR), 'src', 'logs')
-
-# Reset logging
-LOGGING_CONFIG = None
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'default': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': join(LOGFILE_ROOT, 'django.log'),
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            'backupCount': 5,
-            'formatter': 'standard',
-        },
-        'request_handler': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': join(LOGFILE_ROOT, 'requests-django.log'),
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            'backupCount': 5,
-            'formatter': 'standard',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['default'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'django.request': {  # Stop SQL debug from logging to main logger
-            'handlers': ['request_handler'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }
-}
-
-logging.config.dictConfig(LOGGING)
--- src/him_database/settings/production.py
+++ src/him_database/settings/production.py
@@ -1,6 +1,5 @@
 # In production set the environment variable like this:
 #    DJANGO_SETTINGS_MODULE=him_client_database.settings.production
-import logging.config
 
 from .base import *  # NOQA
 
@@ -34,49 +33,4 @@ loaders = [
 TEMPLATES[0]['OPTIONS'].update({"loaders": loaders})
 TEMPLATES[0].update({"APP_DIRS": False})
 
-# Log everything to the logs directory at the top
-LOGFILE_ROOT = join(dirname(BASE_DIR), 'logs')
 
-# Reset logging
-LOGGING_CONFIG = None
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'default': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': join(LOGFILE_ROOT, 'project.log'),
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            'backupCount': 5,
-            'formatter': 'standard',
-        },
-        'request_handler': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': join(LOGFILE_ROOT, 'project.log'),
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            'backupCount': 5,
-            'formatter': 'standard',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['default'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'django.request': {  # Stop SQL debug from logging to main logger
-            'handlers': ['request_handler'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }
-}
-
-logging.config.dictConfig(LOGGING)
